0bd01850d6d806d30d4cce6c69c03001d75c53ec
Olli Lehmuskenttä <olli.lehmuskentta@tuni.fi>
2023-12-11 18:10:03 +0200

    more tests for toNumber

--- test/toNumber.test.js
+++ test/toNumber.test.js
@@ -1,13 +1,22 @@
 import toNumber from "../src/toNumber.js";
 import { assert } from "chai";
 describe("toNumber", () => {
-    it("should convert a decimal string to number", () => {
-        assert.strictEqual(toNumber("105.3"), 105.3);
-    });
-    it('should handle hexadecimal string to number', () => {
-        assert.strictEqual(toNumber('0x1'), 1);
-    });
-    it('should handle binary string to number', () => {
-        assert.strictEqual(toNumber('0b101'), 5);
-    });
+  it("should convert a decimal string to number", () => {
+    assert.strictEqual(toNumber("105.3"), 105.3);
+  });
+  it("should handle hexadecimal string to number", () => {
+    assert.strictEqual(toNumber("0x1"), 1);
+  });
+  it("should handle binary string to number", () => {
+    assert.strictEqual(toNumber("0b101"), 5);
+  });
+  it("should handle null input", () => {
+    const result = toNumber(null);
+    assert.strictEqual(result, 0, "Failed to handle null input");
+  });
+
+  it("should handle undefined input", () => {
+    const result = toNumber(undefined);
+    assert.isNaN(result, "Undefined input should result in NaN");
+  });
 });
